f1116901ba2306beae3fe4e26d474043da27cf36
AntonBeYt <143318322+AntonBeYt@users.noreply.github.com>
2024-09-23 15:09:01 +0200

    created header component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 
 import Link from "next/link";
+import { Header } from "./components/Header";
 
 export default function Home() {
   return (
     <>
+      <Header />
       <p>HelloWorld</p>
       <Link href={"/AddProduct"}>Lägg till produkt</Link>
     </>
